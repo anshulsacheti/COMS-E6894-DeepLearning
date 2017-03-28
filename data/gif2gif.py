1b511006a05c6752dc270dff037f247c2ce710df
anshulsacheti <anshul.sacheti@gmail.com>
2017-03-27 22:25:25 -0400

    Fixed gif->pngs with additional -coalesce option for alpha values. Added comments to functions. Changed functionality to input tag on command line instead of updating file. Added new defs to handle parsing args and getting shared vars created.

--- data/gif2gif.py
+++ data/gif2gif.py
@@ -2,29 +2,62 @@ from subprocess import call
 import glob
 import os
 import shutil
+import argparse
 from PIL import Image
 
 
-tag="cat"
-recovered_path="recovered_gifs_"+tag
+def initConversion(tag):
+	"""
+	Creates some paths that are shared between defs
 
-split_path="./gifs/"+tag+"/"
-gif_path=glob.glob(split_path+"*.gif")
+	tag - folder name to look in for gifs
+	"""
 
-gif_path.sort(key=lambda f: int(filter(str.isdigit, f)))
+	#tag="dog2"
+	recovered_path="recovered_gifs_"+tag
 
-def gif2images():
+	split_path="./gifs/"+tag+"/"
+	gif_path=glob.glob(split_path+"*.gif")
+	gif_path.sort(key=lambda f: int(filter(str.isdigit, f)))
+
+	return (recovered_path, split_path, gif_path)
+
+def parse_args():
+	"""
+	Handles input of tag/folder name on command line
+	"""
+	parser = argparse.ArgumentParser(description="Generate pngs-gifs")
+	parser.add_argument('tag', nargs='+', help="Tags of GIFs to convert to PNG")
+	return parser.parse_args()
+
+def gif2images(tag,split_path,gif_path):
+	"""
+	Converts a gif to pngs with each png containing the full image data
+
+	tag - folder name where files reside
+	split_path - path to folder where gifs are stored
+	gif_path - path of all gifs in split_path
+	"""
 	for i in range(len(gif_path)):
 		if not os.path.exists(split_path+"/"+str(i)):
 			os.mkdir(split_path+"/"+str(i))
 	for idx, gif in enumerate(gif_path):
 		print("Splitting %d.gif with tag '%s'"%(idx,tag))
 		# May need to modify the path.
-		call(["convert",gif_path[idx],"gifs/"+tag+"/"+str(idx)+"/%05d.png"])
-	print "Done with splitting." 
-def images2gif():
+		call(["convert","-coalesce",gif_path[idx],"gifs/"+tag+"/"+str(idx)+"/%05d.png"])
+	print "Done with splitting."
+
+def images2gif(tag,recovered_path,gif_path):
+	"""
+	Converts a png frames to gif with each png containing the full image data
+
+	tag - folder name where files reside
+	recovered_path - path to folder where generated gifs are stored
+	gif_path - path of all gifs in split_path
+	"""
+
 	if os.path.exists(recovered_path):
-		boo = raw_input("recovered gifs already exist, overwrite?y/n")
+		boo = raw_input("recovered gifs already exist, overwrite? : y/n -> ")
 		if boo is 'y':
 			shutil.rmtree(recovered_path)
 			os.mkdir(recovered_path)
@@ -39,7 +72,11 @@ def images2gif():
 		delay = original_gif.info['duration']/10.0
 		# May need to modify the paths.
 		call(["convert","-delay",str(delay),"-loop","0",png_path+"/000*.png",recovered_path+"/"+str(i)+".gif"])
-	print "Done with recovering." 
+	print "Done with recovering."
+
 if __name__ == '__main__':
-	gif2images()
-	images2gif()
+	args = parse_args()
+	tag = args.tag[0]
+	recovered_path, split_path, gif_path = initConversion(tag)
+	gif2images(tag,split_path,gif_path)
+	images2gif(tag,recovered_path,gif_path)
